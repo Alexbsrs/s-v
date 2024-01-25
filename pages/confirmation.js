01c7a57ded0f5a4868406ac66ecc6d5a0059bbb1
Alexandre Bensarsa <alexandre.bensarsa@jeece.fr>
2024-01-25 11:12:31 +0100

    pretty final product

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,4 +1,3 @@
-// pages/confirmation.js
 import Head from 'next/head';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -11,12 +10,7 @@ export default function Confirmation() {
   useEffect(() => {
     const container = document.getElementById('fireworks');
     const options = {
-      maxRockets: 3,            // trois fusées à la fois
-      rocketSpawnInterval: 150, // nouvelle fusée toutes les 150 ms
-      numParticles: 100,        // 100 particules pour chaque explosion
-      explosionMinHeight: 0.2,  // hauteur minimale de l'explosion : 20%
-      explosionMaxHeight: 0.9,  // hauteur maximale de l'explosion : 90%
-      explosionChance: 0.08     // chance de 8 % pour chaque fusée d'exploser
+      // vos options de feux d'artifice...
     };
     const fireworks = new Fireworks(container, options);
     fireworks.start();
@@ -34,18 +28,13 @@ export default function Confirmation() {
   const toggleSound = () => {
     const audio = document.getElementById('background-music');
     if (audio) {
-      if (isMuted) {
-        audio.muted = false;
-        setIsMuted(false);
-      } else {
-        audio.muted = true;
-        setIsMuted(true);
-      }
+      isMuted ? audio.muted = false : audio.muted = true;
+      setIsMuted(!isMuted);
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-red-100">
+    <div className="flex items-center justify-center h-screen overflow-hidden bg-gradient-to-b from-pink-100 via-pink-200 to-pink-300">
       <Head>
         <title>Confirmation</title>
       </Head>
@@ -53,18 +42,18 @@ export default function Confirmation() {
       <audio id="background-music" src="/musique.mp3" loop autoPlay></audio>
       <button 
         onClick={toggleSound} 
-        className="absolute top-4 left-4 z-50 text-xl text-white bg-blue-500 p-2 rounded"
+        className={`absolute top-4 left-4 z-50 text-xl text-white p-2 rounded-full shadow-md focus:outline-none focus:ring-2 ${isMuted ? 'bg-green-500 focus:ring-green-400' : 'bg-red-500 focus:ring-red-400'}`}
       >
         {isMuted ? 'Activer le son' : 'Couper le son'}
       </button>
       <div className="text-center z-10">
-        <h1 className="text-4xl text-red-600 font-bold mb-4">
+        <h1 className="text-6xl text-red-600 font-bold mb-4">
           TROP BIEN WOULA
         </h1>
-        <Link href="/" passHref>
-          <span className="text-xl text-blue-500 hover:underline cursor-pointer">
+        <Link href="/">
+          <button className="text-xl text-pink-500 hover:text-pink-600 hover:underline">
             Retourner à l'accueil
-          </span>
+          </button>
         </Link>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-// pages/saint-valentin.js
 import Head from 'next/head';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import '../styles/globals.css';
 
 export default function SaintValentin() {
@@ -41,54 +41,120 @@ export default function SaintValentin() {
   };
 
   return (
-    <div className="smiley-background flex items-center justify-center h-screen overflow-hidden">
-      <Head>
-        <title>Veux-tu être ma Valentine ?</title>
-      </Head>
-      <button 
-        onClick={playMusic} 
-        className="absolute top-2 left-4 z-50 text-l text-white bg-green-500 p-2 rounded"
-      >
-        Lancer la musique
-      </button>
-      <button 
-        onClick={stopMusic} 
-        className="absolute top-14 left-4 z-50 text-l text-white bg-red-500 p-2 rounded"
-      >
-        Couper le son
-      </button>
-      <div className="text-center">
-        <h1 className="text-4xl text-red-600 font-bold mb-4">
-          Chère Canelle,
-        </h1>
-        <h1 className="text-4xl text-red-600 font-bold mb-4">
-          Veux-tu être ma Valentine pour la Saint-Valentin ?
-        </h1>
-        <button 
-          onClick={handleYesClick}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-6 text-2xl rounded"
-        >
-          Oui, je le veux !
-        </button>
-        <button 
-          onClick={handleNoClick}
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 text-2xl rounded ml-4"
-        >
-          Non, flemme
-        </button>
+    <>
+      <div className="fixed bottom-40 right-40">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
       </div>
-      {showPopup && (
-        <>
-          <div className="fixed inset-0 bg-black bg-opacity-50 z-10" aria-hidden="true"></div>
-          <div className="fixed inset-0 z-20 flex items-center justify-center p-4">
-            <div className="bg-white p-6 rounded shadow-lg max-w-lg mx-auto text-center">
-              <p className="text-red-500 text-xl font-bold">Mauvaise réponse !</p>
-              <p className="mb-4">Nouvel essai dans : {countdown}s</p>
+      <div className="fixed top-20 right-20">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="fixed top-40 left-40">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="fixed top-20 left-80">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="fixed top-10 right-80">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="fixed bottom-20 left-40">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="fixed bottom-10 left-80">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="fixed bottom-10 right-80">
+        <Image
+          src="/smiley_bg.jpg" 
+          alt="Smiley Background"
+          width={100}  // Spécifiez une largeur
+          height={100} // Spécifiez une hauteur
+        />
+      </div>
+      <div className="flex items-center justify-center h-screen overflow-hidden bg-gradient-to-b from-pink-100 via-pink-200 to-pink-300">
+        <Head>
+          <title>Veux-tu être ma Valentine ?</title>
+        </Head>
+        <button 
+          onClick={playMusic} 
+          className="absolute top-2 left-4 z-50 text-l text-white bg-green-500 p-2 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+        >
+          Lancer la musique
+        </button>
+        <button 
+          onClick={stopMusic} 
+          className="absolute top-14 left-4 z-50 text-l text-white bg-red-500 p-2 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75"
+        >
+          Couper le son
+        </button>
+        <div className="text-center">
+          <h1 className="text-pink-600 font-bold text-5xl mb-4">
+            Chère Canelle,
+          </h1>
+          <h1 className="text-red-600 font-bold text-5xl mb-6">
+            Veux-tu être ma Valentine pour la Saint-Valentin ?
+          </h1>
+          <button 
+            onClick={handleYesClick}
+            className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-opacity-75 text-2xl transition duration-300 ease-in-out"
+          >
+            Oui, je le veux !
+          </button>
+          <button 
+            onClick={handleNoClick}
+            className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 text-2xl transition duration-300 ease-in-out ml-4"
+          >
+            Non, flemme
+          </button>
+        </div>
+        {showPopup && (
+          <>
+            <div className="fixed inset-0 bg-black bg-opacity-50 z-10" aria-hidden="true"></div>
+            <div className="fixed inset-0 z-20 flex items-center justify-center p-4">
+              <div className="bg-white p-6 rounded-2xl shadow-lg max-w-lg mx-auto text-center">
+                <p className="text-red-500 text-xl font-bold">Mauvaise réponse !</p>
+                <p className="mb-4">Nouvel essai dans : {countdown}s</p>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-      <audio id="background-music" src="/test.mp3" loop></audio>
-    </div>
+          </>
+        )}
+        <audio id="background-music" src="/test.mp3" loop></audio>
+      </div>
+    </>
   );
 }
